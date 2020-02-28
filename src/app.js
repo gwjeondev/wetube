1925270fc767f5161a80a40df059e9243abdb96a
root <root@DESKTOP-2R94GT0.localdomain>
2020-02-28 23:54:03 +0900

    502 error

--- src/app.js
+++ src/app.js
@@ -52,9 +52,9 @@ app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 app.use(routes.api, apiRouter);
 
-app.use((req, res, next) => {
-  res.status(404);
-  res.render("error404");
-});
+// app.use((req, res, next) => {
+//   res.status(404);
+//   res.render("error404");
+// });
 
 export default app;
